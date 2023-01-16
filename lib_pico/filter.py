8c2f0fffca3a8a2ae5b36cf87cbdff7ea3d91cd2
xiansnn <xiansnn@hotmail.com>
2023-01-16 11:57:48 +0100

    update PID filter

--- lib_pico/filter.py
+++ lib_pico/filter.py
@@ -4,17 +4,16 @@ def clamp(min_value, max_value, value):
 class Filter():
     '''
 coef_in : the list of n coef values in [0 ... n-1] for the n input values including present value (x[0])
-coef_out : the list of m coef values in [1 ... m] for the output values in [1 ... m].
+coef_out : the list of m coef values in [0 ... m] for the output values in [0 ... m] with always coef_out[0]=1.
 By definition,the coeficient of computed present output Y[0] is 1.
 note : size of input vector is n+1,  size of output vector is m
 '''
     def __init__(self, coef_in, coef_out):
-        self._size_in = len(coef_in)
-        self._X = [0]*self._size_in
-        self._size_out = len(coef_out)
-        self._Y = [0]*self._size_out
         self._A = coef_in
         self._B = coef_out
+        if (self._B[0]!=1): print("warning : coef_out[0]!=1")
+        self._X = [0]*len(self._A)
+        self._Y = [0]*len(self._B)
         
     def __repr__(self):
         return f"A[i]:{self._A}, B[i]:{self._B}"
@@ -27,13 +26,12 @@ note : size of input vector is n+1,  size of output vector is m
         self._push(self._X, x_in) # init X[0]
         self._push(self._Y, 0) # init Y[0]
         
-        for i in range(self._size_in):
+        for i in range(len(self._A)):
             self._Y[0] += self._A[i]*self._X[i]
             
-        for j in range(1,self._size_out):
+        for j in range(1,len(self._B)):
             self._Y[0] += self._B[j]*self._Y[j]
             
-        self._Y[0] = self._Y[0]/self._B[0]
         return self._Y[0]
     
     def get_output(self):
@@ -44,9 +42,9 @@ note : size of input vector is n+1,  size of output vector is m
 class PID(Filter):
     def __init__(self, Ts, G, Ti, Td):
         A = [0]*3
-        A[0] = G + Ts/Ti + Td/Ts
-        A[1] = - (G + 2*Td/Ts)
-        A[2] = Td/Ts
+        A[0] = G*(1 + Ts/Ti + Td/Ts)
+        A[1] = - G*(1 + 2*Td/Ts)
+        A[2] = G*Td/Ts
         B = [0]*2
         B[0] = 1
         B[1] = 1
@@ -60,14 +58,14 @@ class FilteredPID(Filter):
         a = Ti/Ts
         b = Td/Ts
         c = b/N
-        d = a*G + c
-        e = a*c*G + a*b
+        d = a + c
+        e = a*c + a*b
         f = a+a*c
                 
         A = [0]*3
-        A[0] = (1 + d + e)/f
-        A[1] = - (d + 2*e)/f
-        A[2] = e/f
+        A[0] = G*(1 + d + e)/f
+        A[1] = - G*(d + 2*e)/f
+        A[2] = G*e/f
         
         B = [0]*3
         B[0] = 1
@@ -92,16 +90,16 @@ class Means(Filter):
 
     
 if __name__ == "__main__":
-    nb_point = 30
+    nb_point =30
     def test_PID():
-        pid = PID(Ts=1000, Td=20, Ti=3000, G=.5)
+        pid = PID(Ts=1000, Td=10, Ti=1500, G=.6)
 #         print(pid)
         setpoint = 1000
         for _ in range(nb_point):
             delta = setpoint - pid.get_output()
             print( pid.filter(delta))
     def test_FileteredPID():    
-        fpid = FilteredPID(Ts=1000, Td=20, Ti=3000, G=.5, N=10)
+        fpid = FilteredPID(Ts=1000, Td=1, Ti=1500, G=.6, N=1)
 #         print(fpid)
         setpoint = 1000
         for _ in range(nb_point):
@@ -109,7 +107,7 @@ if __name__ == "__main__":
             print( fpid.filter(delta))
     def test_Means():     
         m = Means(5)
-        print(m)
+#         print(m)
         setpoint = 1000
         for _ in range(nb_point):
             delta = setpoint - m.get_output()
@@ -117,8 +115,8 @@ if __name__ == "__main__":
             
 
 #     test_PID()
-#     test_FileteredPID()
-    test_Means()
+    test_FileteredPID()
+#     test_Means()
 
         
         
