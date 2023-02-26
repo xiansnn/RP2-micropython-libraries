9c3f872d16f2bcfaf4c383de56848c87e8cac435
xiansnn <xiansnn@hotmail.com>
2023-02-26 17:34:51 +0100

    add probes ID for logic analyser

--- debug_utility/pulses.py
+++ debug_utility/pulses.py
@@ -2,6 +2,7 @@ from machine import Pin, Signal
 import utime
 
 
+
 class Probe():
     """
 A Class that provides means for events observation on oscilloscope.
@@ -38,90 +39,64 @@ Usefull for debugging.
             self.probe.off()
             utime.sleep_us(500)
 
+# wiring of Logic Analyzer
+D0 = Probe(27)#blanc  ch1
+D1 = Probe(16)#brun   ch2
+D2 = Probe(17)#rouge  ch3
+D3 = Probe(18)#orange ch4
+D4 = Probe(19)#jaune  ch5
+D5 = Probe(20)#vert   ch6
+D6 = Probe(21)#bleu   ch7
+D7 = Probe(26)#violet ch8
 
 
 
-
-def test():
-    probe_gpio = 16 # a single  probe test 
-    p = Probe(probe_gpio)
-    
-    while (True):
-
-        for n in range (10):
-            p.on()
-            p.off()
-            # time on -> off = 33 us
-            utime.sleep_ms(1)
-
-        utime.sleep(1)
-        
-        for n in range (10):
-            p.pulses(n)
-            # time n=1 on -> off = 7 us
-            # time n=2 on -> off = 9 us
-            # time n=10 on -> off = 8 us
-            
-
-        utime.sleep(1)
-
-        for n in range (1,10):
-            p.pulse_single(n)
-            # time n=1 on -> off = 45 us
-            # time n=2 on -> off = 37 us
-            # time n=3 on -> off = 47 us
-            # time n=4 on -> off = 56 us
-            # time n=10 on -> off = 106 us
-
-
-        utime.sleep(1)
-        
-        p.pulse_begin(3) # time on -> off = 3.02 ms 
-        utime.sleep_us(500)
-        p.pulse_end(3) # time on -> off = 3.12 ms
-
-        utime.sleep(2)
-        
-def test2(): # a test with multiple GPIO for logic analyser observation
-    D1 = Probe(16)
-    D2 = Probe(17)
-    D3 = Probe(18)
-    D4 = Probe(19)
-    D5 = Probe(20)
-    D6 = Probe(21)
-    D7 = Probe(22)
-    
-    while True:
-        D1.on()
-        utime.sleep_ms(100)
-        D2.on()
-        utime.sleep_ms(100)
-        D3.on()
-        utime.sleep_ms(100)
-        D4.on()
-        utime.sleep_ms(100)
-        D5.on()
-        utime.sleep_ms(100)
-        D6.on()
-        utime.sleep_ms(100)
-        D7.on()
-        utime.sleep_ms(100)
-        D7.off()
-        utime.sleep_ms(100)
-        D6.off()
-        utime.sleep_ms(100)
-        D5.off()
-        utime.sleep_ms(100)
-        D4.off()
-        utime.sleep_ms(100)
-        D3.off()
-        utime.sleep_ms(100)
-        D2.off()
-        utime.sleep_ms(100)
-        D1.off()
-        utime.sleep_ms(100)
-        
     
 
 if __name__ == "__main__":
+    def test():
+        probe_gpio = 16 # a single  probe test 
+        p = Probe(probe_gpio)
+        
+        while (True):
+
+            for n in range (10):
+                p.on()
+                p.off()
+                # time on -> off = 33 us
+                utime.sleep_ms(1)
+
+            utime.sleep(1)
+            
+            for n in range (10):
+                p.pulses(n)
+                # time n=1 on -> off = 7 us
+                # time n=2 on -> off = 9 us
+                # time n=10 on -> off = 8 us
+            utime.sleep(1)
+
+            for n in range (1,10):
+                p.pulse_single(n)
+                # time n=1 on -> off = 45 us
+                # time n=2 on -> off = 37 us
+                # time n=3 on -> off = 47 us
+                # time n=4 on -> off = 56 us
+                # time n=10 on -> off = 106 us
+            utime.sleep(1)
+            
+            p.pulse_begin(3) # time on -> off = 3.02 ms 
+            utime.sleep_us(500)
+            p.pulse_end(3) # time on -> off = 3.12 ms
+            utime.sleep(2)
+        
+    def test2(): # a test with multiple GPIO for logic analyser observation
+        delay = const(100)
+        probes = [D0, D1, D2, D3, D4, D5, D6, D7]
+        while True:
+            utime.sleep_ms(delay*len(probes))
+            for p in probes:
+                p.on()
+                utime.sleep_ms(delay)
+                p.off()
+        
     test2()
